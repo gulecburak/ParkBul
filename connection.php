89e54d8e4ef09e03b39b7affa3fc999daafaadca
gulecburak <51175548+gulecburak@users.noreply.github.com>
2019-05-30 22:51:07 +0300

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+define('hostname','localhost');
+define('user','root');
+define('password','');
+define('databasename','hello');
+$connect=mysqli_connect(hostname,user,password,databasename);
+
+
+?>
